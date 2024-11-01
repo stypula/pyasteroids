d33dcf431ccbb081648e265538f1054c6f3e12eb
stypula <pstypula92@gmail.com>
2024-11-01 01:51:23 -0500

    b: added player; circleshape; added implementation to main. Player can move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-
+from player import *
 def main():
     pygame.init()
     print("Starting asteroids!")
@@ -8,12 +8,32 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+   
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while ( True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        for updateMe in updatable:
+            updateMe.update(dt)
+        for drawMe in drawable:
+            drawMe.draw(screen)
+       
         pygame.display.flip()
+        time = clock.tick(60)
+        dt = time/1000
+       
 
 if __name__ == "__main__":
     main()
